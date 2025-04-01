926858b3796d93dbfaf37c8ed552106f55ff7a72
jeandjogo857 <jeandjogo857@gmail.com>
2025-04-01 15:12:44 +0000

    Add 21.ts

---
+++ src/21.ts
@@ -0,0 +1,13 @@
+// 代码示例1 - 使用函数
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+// 代码示例2 - 使用箭头函数
+const multiply = (a: number, b: number) => {
+    return a * b;
+};
+
+// 代码示例3 - 使用数组操作
+const arrayOperations = [10, 5, 7];
+console.log(arrayOperations.map(item => item * 2));
